2c06a0bdeffb732bcb0ff94010fc03322954559a
Douglas Araújo <devdoug45@gmail.com>
2023-09-18 22:00:55 -0300

    configura a migration de posts_categories

--- src/migrations/20230919004456-create-posts_categories.js
+++ src/migrations/20230919004456-create-posts_categories.js
@@ -8,6 +8,30 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      category_id: {
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +41,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    await queryInterface.dropTable('posts_categories');
+  },
 };
